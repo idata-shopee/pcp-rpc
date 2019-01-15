d9d624a3c56171b0b6690d72e642c81a5519e39e
loveKino <842914439@qq.com>
2019-01-15 12:23:31 +0800

    add exception case

--- src/test/scala/io/github/shopee/idata/pcp-rpc/PcpRpcTest.scala
+++ src/test/scala/io/github/shopee/idata/pcp-rpc/PcpRpcTest.scala
@@ -21,6 +21,11 @@ class PcpRpcTest extends org.scalatest.FunSuite {
           123
         }
       }),
+      "textException" -> Sandbox.toSanboxFun((params: List[Any], pcs: PcpServer) => {
+        TimeoutScheduler.sleep(100) map { _ =>
+          throw new Exception("test rpc exception.")
+        }
+      })
     )
   )
 
@@ -46,6 +51,33 @@ class PcpRpcTest extends org.scalatest.FunSuite {
     server.close()
   }
 
+  def testCallRpcServerFail(list: CallResult, clientNum: Int = 100) = {
+    val server = PcpRpc.getPCServer(sandbox = sandbox)
+
+    val clientCall = () => {
+      val client = Await.result(
+        PcpRpc.getPCClient(
+          port = server.getLocalAddress().asInstanceOf[InetSocketAddress].getPort()
+        ),
+        15.seconds
+      )
+      var count = 0
+      client.call(list) recover {
+        case e: Exception => {
+          count += 1
+        }
+      } map { _ =>
+        assert(count == 1)
+      }
+    }
+
+    Await.result(Future.sequence(1 to clientNum map { _ =>
+      clientCall()
+    }), 15.seconds)
+
+    server.close()
+  }
+
   test("base") {
     val p = new PcpClient()
     testCallRpcServer(p.call("add", 1, 2), 3)
@@ -54,5 +86,10 @@ class PcpRpcTest extends org.scalatest.FunSuite {
   test("future") {
     val p = new PcpClient()
     testCallRpcServer(p.call("testFuture"), 123)
+  }
+
+  test("exception") {
+    val p = new PcpClient()
+    testCallRpcServerFail(p.call("testException"))
   }
 }
